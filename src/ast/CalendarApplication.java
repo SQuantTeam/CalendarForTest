71bba3875be0f1b2cadcf6d1b12f19711d32fb39
OptimusPrime1997 <874855266@qq.com>
2018-12-25 20:13:01 +0800

    change war packing to jar

--- src/ast/CalendarApplication.java
+++ src/ast/CalendarApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.filter.HttpPutFormContentFilter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -19,17 +15,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
                 DataSourceAutoConfiguration.class,
                 DataSourceTransactionManagerAutoConfiguration.class,
                 HibernateJpaAutoConfiguration.class})
-@EnableTransactionManagement
-@EnableScheduling
+//@EnableTransactionManagement
+//@EnableScheduling
 @EnableSwagger2
 @Slf4j
 public class CalendarApplication {
     public static void main(String[] args) {
         SpringApplication.run(CalendarApplication.class, args);
-    }
-
-    @Bean
-    public HttpPutFormContentFilter httpPutFormContentFilter() {
-        return new HttpPutFormContentFilter();
     }
 }
